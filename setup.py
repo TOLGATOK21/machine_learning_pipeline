7fd234f320b95ca7cf8dcea7f4dc56bd3c3903a8
TOLGATOK21 <tolgatnh@gmail.com>
2025-06-06 03:32:47 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+# This setup.py will be responsible in creating by machine learning application as a package
+
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.1.0',
+    author='tolga_tok',
+    author_email='tolgatnh@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+    
+    
+    )
